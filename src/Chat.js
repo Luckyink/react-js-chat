4ab6cb5e37fc95ee4d108e4ed4308de49accbd2a
Luckyink <saidmakono1@gmail.com>
2022-10-20 16:24:51 +0300

    add Chat component

---
+++ src/Chat.js
@@ -0,0 +1,11 @@
+import React, { useState, useCallback } from 'react';
+
+const Chat = () => {
+    return (
+        <div class="card chat-app">
+          
+        </div>
+      );
+}
+
+export default React.memo(Chat);

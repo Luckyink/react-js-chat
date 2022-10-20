58308d7b0d0aeaad2c5bc29f96179a924f13c1a6
Luckyink <saidmakono1@gmail.com>
2022-10-20 17:51:40 +0300

    add person item component

---
+++ src/components/PeopleItem.js
@@ -0,0 +1,15 @@
+import React, { useCallback, useRef } from 'react';
+
+const PeopleItem = ({ username, id}) => {
+  return (
+      <li class="clearfix">
+          <img src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="avatar" />
+              <div class="about">
+                <div class="name">{username}</div>
+                <div class="status"> <i class="fa fa-circle online"></i></div>                                            
+              </div>
+      </li>
+  )
+};
+
+export default React.memo(PeopleItem);

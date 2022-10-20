1a15ca74d04d38e64f127c0760539f4951da6946
Luckyink <saidmakono1@gmail.com>
2022-10-20 17:27:59 +0300

    add message post area on chat area component

--- src/components/ChatArea.js
+++ src/components/ChatArea.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ChatHistory from './ChatHistory';
+import PostMessage from './PostMessage';
 
 // COMPONENT
 const ChatArea = ({chatHistory,onPostMessage}) => {
   return (
       <div class="chat">
         <ChatHistory chatHistory={chatHistory}/>
+        <PostMessage onPostMessage = {onPostMessage} />
       </div>
   )
 };
